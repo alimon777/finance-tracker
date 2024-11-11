ed98f607a2f977034dc235a58e76c453246f7cd7
Sidharth-A-691 <sidharth69122@gmail.com>
2024-11-11 14:10:18 +0530

    UI changes made - service files

--- Frontend/finwiz/src/app/service/custom/custom.interceptor.ts
+++ Frontend/finwiz/src/app/service/custom/custom.interceptor.ts
@@ -5,20 +5,20 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class CustomInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // const token = localStorage.getItem('token');
+    const token = localStorage.getItem('token');
 
-    // // Check if the request URL matches a specific pattern
-    // if (req.url.includes('/api/')) { // Adjust the condition as needed
-    //   if (token) {
-    //     const clonedRequest = req.clone({
-    //       setHeaders: {
-    //         Authorization: `Bearer ${token}`,
-    //         'Content-Type': 'application/json',
-    //       }
-    //     });
-    //     return next.handle(clonedRequest);
-    //   }
-    // }
+    // Check if the request URL matches a specific pattern
+    if (req.url.includes('/api/')) { // Adjust the condition as needed
+      if (token) {
+        const clonedRequest = req.clone({
+          setHeaders: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          }
+        });
+        return next.handle(clonedRequest);
+      }
+    }
     
     // Proceed without modifying the request
     return next.handle(req);
